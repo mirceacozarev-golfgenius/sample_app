40cadd62bf709ad03c78144fa88c0c8910ce42fb
Mircea-George Cozarev <mircea.cozarev@golfgenius.com>
2022-07-19 14:51:46 +0300

    Create login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      # redirect_to user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
