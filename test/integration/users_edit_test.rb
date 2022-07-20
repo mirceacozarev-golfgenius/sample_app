a8f3b9f888dda601f1ddc124f10c38168236a82a
Mircea-George Cozarev <mircea.cozarev@golfgenius.com>
2022-07-20 15:11:44 +0300

    Add edit profile redirecting for not-logged-in users or users not matching the profile

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -18,10 +19,30 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    log_in_as(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
     patch user_path(@user), params: { user: { name: name,
                                               email: email,
                                               password: "",
