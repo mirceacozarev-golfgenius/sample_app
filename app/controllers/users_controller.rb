402a95049259130e9e8e451887a712ef000e2706
Mircea-George Cozarev <mircea.cozarev@golfgenius.com>
2022-07-20 09:03:19 +0300

    Create unsuccessful edit behaviour

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,16 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update
+    else
+      render 'edit'
+    end
   end
 
   private
