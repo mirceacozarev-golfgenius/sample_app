b7b2208ca73d67aee3c4e882050a2e85a8464441
Mircea-George Cozarev <mircea.cozarev@golfgenius.com>
2022-07-21 16:47:19 +0300

    Create password reset page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -8,16 +8,16 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
-                    length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
   has_secure_password
   validates :password, length: { minimum: 6 }, presence: true, allow_nil: true
 
   # Returns the hash digest of the given string
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ?  BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -44,17 +44,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # Converts email to all lower-case.
-  def downcase_email
-    email.downcase!
-  end
-
-  # Creates and assigns the activation token and digest.
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
-
   # Activates an account.
   def activate
     update_columns(activated: true, activated_at: true)
@@ -64,4 +53,29 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
